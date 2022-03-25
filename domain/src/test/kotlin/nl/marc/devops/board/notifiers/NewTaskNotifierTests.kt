16c29e9d6ca1b3834e1cc48c8ed974de97500d18
Marc <16156117+Marc-JB@users.noreply.github.com>
2022-03-25 11:19:33 +0100

    Add a few more tests

--- domain/src/test/kotlin/nl/marc/devops/board/notifiers/NewTaskNotifierTests.kt
+++ domain/src/test/kotlin/nl/marc/devops/board/notifiers/NewTaskNotifierTests.kt
@@ -36,6 +36,30 @@ class NewTaskNotifierTests {
         verify(exactly = INVOCATION_KIND_ONCE) { notificationService.sendNotification(any(), any(), users.last()) }
     }
 
+    @Test
+    fun `AD-20, AD-142) Notifications should be sent to all users when a task is moved back`() {
+        // Arrange
+        val getUserByRoleService = mockk<GetUserByRoleService>()
+        val users = UsersFixture.generateUsers(2).toSet()
+        every { getUserByRoleService.getUsersByRole(any()) } returns users
+
+        val notificationService = mockk<NotificationService>()
+        justRun { notificationService.sendNotification(any(), any(), any()) }
+
+        val notifier = NewTaskNotifier(getUserByRoleService, notificationService)
+
+        val task = Task()
+        task.title = "Test notifications"
+
+        // Act
+        notifier.onTaskMovedBack(task, Role.DEVELOPERS, Role.TESTER)
+
+        // Assert
+        verify(exactly = INVOCATION_KIND_ONCE) { getUserByRoleService.getUsersByRole(Role.TESTER) }
+        verify(exactly = INVOCATION_KIND_ONCE) { notificationService.sendNotification(any(), any(), users.first()) }
+        verify(exactly = INVOCATION_KIND_ONCE) { notificationService.sendNotification(any(), any(), users.last()) }
+    }
+
     companion object {
         const val INVOCATION_KIND_ONCE = 1
     }
