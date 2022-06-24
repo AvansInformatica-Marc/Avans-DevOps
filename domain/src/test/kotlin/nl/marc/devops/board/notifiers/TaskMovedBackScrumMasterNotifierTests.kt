c8015d66dcc76d304de625eb51225abf710301bd
Marc <16156117+Marc-JB@users.noreply.github.com>
2022-06-24 15:10:12 +0200

    Add missing test

--- domain/src/test/kotlin/nl/marc/devops/board/notifiers/TaskMovedBackScrumMasterNotifierTests.kt
+++ domain/src/test/kotlin/nl/marc/devops/board/notifiers/TaskMovedBackScrumMasterNotifierTests.kt
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test
 
 class TaskMovedBackScrumMasterNotifierTests {
     @Test
-    fun `FR-1_10) Notifications should be sent to the scrum master`() {
+    fun `FR-1_10) The scrum master should be notified when a task has been moved backward`() {
         // Arrange
         val user = UsersFixture.defaultUser
 
@@ -30,8 +30,29 @@ class TaskMovedBackScrumMasterNotifierTests {
         // Assert
         verify(exactly = INVOCATION_KIND_ONCE) { notificationService.sendNotification(any(), any(), user) }
     }
+    @Test
+    fun `FR-1_10) The scrum master should not be notified when a task has been moved forward`() {
+        // Arrange
+        val user = UsersFixture.defaultUser
+
+        val notificationService = mockk<NotificationService>()
+        justRun { notificationService.sendNotification(any(), any(), any()) }
+
+        val notifier = TaskMovedBackScrumMasterNotifier(user, notificationService)
+
+        val task = Task()
+        task.title = "Test notifications"
+
+        // Act
+        notifier.notify(TaskStateChange(task, Role.TESTER, Role.DEVELOPERS, false))
+
+        // Assert
+        verify(exactly = INVOCATION_KIND_NEVER) { notificationService.sendNotification(any(), any(), user) }
+    }
 
     companion object {
+        const val INVOCATION_KIND_NEVER = 0
+
         const val INVOCATION_KIND_ONCE = 1
     }
 }
